0df9d65d3dd68e03dd877535049c043e7ba7725b
Katie Vogel <kvog983@gmail.com>
2023-09-24 13:45:50 -0400

    added url patterns per views file

--- survey/urls.py
+++ survey/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("",views.index, name = "index")
+    path("",views.index, name = "index"),
+    path("<int:survey_question_id>/detail", views.detail, name="detail"),
+    path("<int:survey_question_id>/results/", views.results, name="results"),
+    path("<int:survey_question_id>/choose_answer/", views.choose_answer, name="choose_answer"),
     ]
