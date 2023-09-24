ac646ec93c9fce41edf1532bf9d8303bbff37a9f
Katie Vogel <kvog983@gmail.com>
2023-09-24 09:49:51 -0400

    initial setup of models for survey and users

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,25 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+
+class Friend(models.Model):
+    '''
+    Class that manages the user instances, or 'friends'.
+    '''
+    username = models.CharField(max_length = 50)
+    user_image = models.URLField()
+    survey_results = models.JSONField()
+
+class SurveyQuestion(models.Model):
+    '''
+    Class that manages the survey questions for friend matching
+    '''
+    question_text = models.CharField(max_length = 250)
+
+class SurveyChoice(models.Model):
+    '''
+    Class that manages the possible responses for survey questions. Value is used to quantify survey results for friend matching.
+    '''
+    question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length = 250)
+    choice_value = models.IntegerField(default = 0)
