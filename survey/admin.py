982369dba46d7e394168fedfff352caee2923bb1
Katie Vogel <kvog983@gmail.com>
2023-09-24 13:44:40 -0400

    add models to admin file for control in Django Admin

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SurveyQuestion, SurveyChoice, Friend
+
+admin.site.register(SurveyQuestion)
+admin.site.register(SurveyChoice)
+admin.site.register(Friend)
