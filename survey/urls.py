b80beff6d23c6ce94c89605048bd10d07c946e06
Katie Vogel <kvog983@gmail.com>
2023-09-30 16:10:26 -0400

    update to url paths

--- survey/urls.py
+++ survey/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
+app_name = "survey"
 urlpatterns = [
     path("",views.index, name = "index"),
-    path("<int:survey_question_id>/detail", views.detail, name="detail"),
+    path("<int:survey_question_id>/", views.detail, name="detail"),
     path("<int:survey_question_id>/results/", views.results, name="results"),
     path("<int:survey_question_id>/choose_answer/", views.choose_answer, name="choose_answer"),
     ]
